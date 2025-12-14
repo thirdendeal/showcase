11c4b75c7280e05a6ce35c0103271c4ad675fe7f
Gabriel José <thirdendeal@protonmail.com>
2025-12-14 16:02:17 -0300

    Add read line script

---
+++ node/nodejs.org/readline.js
@@ -0,0 +1,21 @@
+// Read Line
+// ---------------------------------------------------------------------
+//
+// https://nodejs.org/en/learn/command-line/accept-input-from-the-command-line-in-nodejs
+
+// Get input from a readable stream
+
+// ---------------------------------------------------------------------
+
+import readline from "node:readline";
+
+const rl = readline.createInterface({
+  input: process.stdin, // the terminal input, one line at a time
+  output: process.stdout,
+});
+
+rl.question(`What's your name?\n`, (name) => {
+  console.log(`Hi ${name}!`);
+
+  rl.close();
+});
