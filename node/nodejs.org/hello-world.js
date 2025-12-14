6336658b3391801261b3a273f9b2d668d86886af
Gabriel José <thirdendeal@protonmail.com>
2025-12-14 01:37:04 -0300

    Add hello world script

---
+++ node/nodejs.org/hello-world.js
@@ -0,0 +1,19 @@
+// Hello World
+// ---------------------------------------------------------------------
+//
+// https://nodejs.org/en/learn/command-line/run-nodejs-scripts-from-the-command-line
+
+// Node.js
+//
+// An open-source and cross-platform JavaScript runtime environment
+
+// Under the hood
+//
+// Runs the V8 JavaScript engine, the core of Google Chrome, outside of the browser
+// JavaScript is internally compiled by V8 with just-in-time (JIT) compilation to speed up the execution
+// A Node.js app runs in a single process, without creating a new thread for every request
+// WebAssembly, a high-performance language that can be compiled from various languages, is also supported
+
+// ---------------------------------------------------------------------
+
+console.log("Hello, Node.js!");
